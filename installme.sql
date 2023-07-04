dae5700dbe8144e5f8b57be9d9122b166930100a
Jordan.#2139 <jordansilva213@gmail.com>
2023-07-04 01:09:25 -0400

    remove unneeded table

--- installme.sql
+++ installme.sql
@@ -1,103 +1,64 @@
-CREATE DATABASE fivemdutylogs CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+CREATE DATABASE IF NOT EXISTS `fivemdutylogs`;
+USE `fivemdutylogs`;
 
-USE fivemdutylogs;
+CREATE TABLE IF NOT EXISTS `dutylogs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `steam` text COLLATE utf8mb4_unicode_520_ci,
+  `guild` text COLLATE utf8mb4_unicode_520_ci,
+  `server` bigint(20) DEFAULT NULL,
+  `onduty` text COLLATE utf8mb4_unicode_520_ci,
+  `starttime` datetime DEFAULT NULL,
+  `endtime` datetime DEFAULT NULL,
+  `department` text COLLATE utf8mb4_unicode_520_ci,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=217 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci ROW_FORMAT=DYNAMIC;
 
-CREATE TABLE settings(
-    id BIGINT AUTO_INCREMENT,
-    guild TEXT,
-    showPing BOOLEAN DEFAULT 0,
-    showDiscord BOOLEAN DEFAULT 1,
-    showID BOOLEAN DEFAULT 1,
-    altLog TEXT,
-    showPlayerGraph BOOLEAN DEFAULT 1,
-PRIMARY KEY (id)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS `errors` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `err` longtext COLLATE utf8mb4_unicode_520_ci,
+  `guild` text COLLATE utf8mb4_unicode_520_ci,
+  `interaction` text COLLATE utf8mb4_unicode_520_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
-CREATE TABLE servers(
-    id BIGINT AUTO_INCREMENT,
-    ip TEXT,
-    port TEXT,
-    guild TEXT,
-    prefix TEXT,
-    displayVoice TEXT,
-    displayText TEXT,
-    addedBy TEXT,
-    inactive BOOLEAN DEFAULT 0,
-    name TEXT,
-    maxPlayers BIGINT,
-    peakPlayers BIGINT,
-    mid TEXT,
-PRIMARY KEY (id)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS `logchannels` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `guild` text COLLATE utf8mb4_unicode_520_ci,
+  `channelid` text COLLATE utf8mb4_unicode_520_ci,
+  `type` text COLLATE utf8mb4_unicode_520_ci,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci ROW_FORMAT=DYNAMIC;
 
-CREATE TABLE players(
-    id BIGINT AUTO_INCREMENT,
-    steam TEXT,
-    license TEXT,
-    license2 TEXT,
-    xbl TEXT,
-    live TEXT,
-    discord TEXT,
-    fivem TEXT,
-    guild TEXT,
-    playtime BIGINT DEFAULT 0,
-    server BIGINT,
-PRIMARY KEY (id)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `steam` text COLLATE utf8mb4_unicode_520_ci,
+  `license` text COLLATE utf8mb4_unicode_520_ci,
+  `license2` text COLLATE utf8mb4_unicode_520_ci,
+  `xbl` text COLLATE utf8mb4_unicode_520_ci,
+  `live` text COLLATE utf8mb4_unicode_520_ci,
+  `discord` text COLLATE utf8mb4_unicode_520_ci,
+  `fivem` text COLLATE utf8mb4_unicode_520_ci,
+  `guild` text COLLATE utf8mb4_unicode_520_ci,
+  `server` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
-CREATE TABLE errors(
-    id BIGINT AUTO_INCREMENT,
-    err LONGTEXT,
-    guild TEXT,
-    interaction TEXT,
-PRIMARY KEY (id)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS `servers` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ip` text COLLATE utf8mb4_unicode_520_ci,
+  `port` text COLLATE utf8mb4_unicode_520_ci,
+  `guild` text COLLATE utf8mb4_unicode_520_ci,
+  `addedBy` text COLLATE utf8mb4_unicode_520_ci,
+  `inactive` tinyint(1) DEFAULT '0',
+  `name` text COLLATE utf8mb4_unicode_520_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
-CREATE TABLE keybinds(
-    id BIGINT AUTO_INCREMENT,
-    keybind TEXT,
-    description TEXT,
-    guild TEXT,
-    addedBy TEXT,
-PRIMARY KEY (id)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `steam` text COLLATE utf
 
-
-CREATE TABLE patrols(
-    id BIGINT AUTO_INCREMENT,
-    guild TEXT,
-    type BIGINT,
-    time TEXT,
-    preAnnounceTime TEXT,
-    info TEXT,
-    days TEXT,
-    ping TEXT,
-    channel TEXT,
-    last BIGINT,
-    preSent BOOLEAN DEFAULT 0,
-PRIMARY KEY (id)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-
-CREATE TABLE dailyplayers(
-    id BIGINT AUTO_INCREMENT,
-    server BIGINT,
-    one INT DEFAULT 0,
-    two INT DEFAULT 0,
-    three INT DEFAULT 0,
-    four INT DEFAULT 0,
-    five INT DEFAULT 0,
-    six INT DEFAULT 0,
-    seven INT DEFAULT 0,
-    eight INT DEFAULT 0,
-    nine INT DEFAULT 0,
-    ten INT DEFAULT 0,
-    eleven INT DEFAULT 0,
-    twelve INT DEFAULT 0,
-    thirteen INT DEFAULT 0,
-    fourteen INT DEFAULT 0,
-    fithteen INT DEFAULT 0,
-    sixteen INT DEFAULT 0,
-    seventeen INT DEFAULT 0,
-    eightteen INT DEFAULT 0,
-    nineteen INT DEFAULT 0,
-    twenty INT DEFAULT 0,
-    twentyone INT DEFAULT 0,
-    twentytwo INT DEFAULT 0,
-    twentythree INT DEFAULT 0,
-    twentyfour INT DEFAULT 0,
-    lastReset TEXT,
-PRIMARY KEY (id)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+8mb4_unicode_520_ci,
+  `server` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=162 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,85 +1,74 @@
 const { InteractionType } = require('discord.js');
 const { Embed } = require('../structure/backend/build');
-module.exports = async function(client, interaction) {
-    client.db.query(`SELECT * FROM settings WHERE guild = ?;`, [interaction.guild.id], async function(err, settings) {
-        if (!settings?.length) client.db.query(`INSERT INTO settings (guild) VALUES (?);`, [
-            interaction.guild.id
-        ], function() {});
-        interaction.guild.settings = settings?.length ? settings[0] : {
-            guild: interaction.guild.id,
-            showPing: 0,
-            showDiscord: 1,
-            showID: 1
-        };
-        if (interaction.type === InteractionType.ApplicationCommand) {
-            let permCheck = interaction.commandName;
-            if (interaction.options?._group) permCheck += ` ${interaction.options._group}`;
-            if (interaction.options?._subcommand) permCheck += ` ${interaction.options._subcommand}`;
-            const command = await client.commands.get(permCheck);
-            if (!command) return;
-            if (command.info?.perm !== "Everyone" && interaction.member.id !== "802459473612505099") {
-                if (!interaction.member.permissions.has(command.info.perm)) {
-                    let embed = new Embed()
-                        .setAuthor({ name: "Missing Permission", iconURL: client.config.err })
-                        .setDescription(`You are missing the \`${command.info.perm}\` permission required to run this command.`)
-                        .setColor(client.config.red)
-                    return interaction.reply({ embeds: [embed], ephemeral: true });
-                };
+module.exports = async function (client, interaction) {
+    if (interaction.type === InteractionType.ApplicationCommand) {
+        let permCheck = interaction.commandName;
+        if (interaction.options?._group) permCheck += ` ${interaction.options._group}`;
+        if (interaction.options?._subcommand) permCheck += ` ${interaction.options._subcommand}`;
+        const command = await client.commands.get(permCheck);
+        if (!command) return;
+        if (command.info?.perm !== "Everyone" && interaction.member.id !== "802459473612505099") {
+            if (!interaction.member.permissions.has(command.info.perm)) {
+                let embed = new Embed()
+                    .setAuthor({ name: "Missing Permission", iconURL: client.config.err })
+                    .setDescription(`You are missing the \`${command.info.perm}\` permission required to run this command.`)
+                    .setColor(client.config.red)
+                return interaction.reply({ embeds: [embed], ephemeral: true });
             };
-            try {
-                command.run(client, interaction);
-            } catch (e) {
+        };
+        try {
+            command.run(client, interaction);
+        } catch (e) {
+            client.emit("errorEmbed", interaction, e.stack);
+        };
+    };
+    if (interaction.type === InteractionType.MessageComponent) {
+        if (interaction.customId.includes("-")) {
+            let name = interaction.customId.split("-")[0];
+            let component;
+            switch (interaction.componentType) {
+                case 5: // User
+                case 6: // Role
+                case 7: // Mentionable
+                case 8: // Channels
+                case 3: // Text
+                    component = client.selectMenus.get(name);
+                    break;
+                case 2:
+                    component = client.buttons.get(name);
+                    break;
+            };
+            if (!component) return;
+            let id = interaction.customId.split("-").slice(1).join("-");
+            try { component.run(client, interaction, id); } catch (e) {
                 client.emit("errorEmbed", interaction, e.stack);
             };
-        };
-        if (interaction.type === InteractionType.MessageComponent) {
-            if (interaction.customId.includes("-")) {
-                let name = interaction.customId.split("-")[0];
-                let component;
-                switch(interaction.componentType) {
-                    case 5: // User
-                    case 6: // Role
-                    case 7: // Mentionable
-                    case 8: // Channels
-                    case 3: // Text
-                        component = client.selectMenus.get(name);
+        } else {
+            let component;
+            switch (interaction.componentType) {
+                case 5: // User
+                case 6: // Role
+                case 7: // Mentionable
+                case 8: // Channels
+                case 3: // Text
+                    component = client.selectMenus.get(interaction.customId);
                     break;
-                    case 2:
-                        component = client.buttons.get(name);
+                case 2:
+                    component = client.buttons.get(interaction.customId);
                     break;
-                };
-                if (!component) return;
-                let id = interaction.customId.split("-").slice(1).join("-");
-                try { component.run(client, interaction, id); } catch (e) {
-                    client.emit("errorEmbed", interaction, e.stack);
-                };
-            } else {
-                let component;
-                switch(interaction.componentType) {
-                    case 5: // User
-                    case 6: // Role
-                    case 7: // Mentionable
-                    case 8: // Channels
-                    case 3: // Text
-                        component = client.selectMenus.get(interaction.customId);
-                    break;
-                    case 2:
-                        component = client.buttons.get(interaction.customId);
-                    break;
-                };
-                if (!component) return;
-                try { component.run(client, interaction); } catch (e) {
-                    client.emit("errorEmbed", interaction, e.stack);
-                };
-            }
-        };
-        if (interaction.type === InteractionType.ModalSubmit) {
-            let modal = client.modal.get(interaction.customId.split("-")[0]);
-            if (!modal) return;
-            interaction.fields = interaction.fields.fields;
-            try { modal.run(client, interaction, interaction.customId.split("-").slice(1).join("-")); } catch (e) {
+            };
+            if (!component) return;
+            try { component.run(client, interaction); } catch (e) {
                 client.emit("errorEmbed", interaction, e.stack);
             };
+        }
+    };
+    if (interaction.type === InteractionType.ModalSubmit) {
+        let modal = client.modal.get(interaction.customId.split("-")[0]);
+        if (!modal) return;
+        interaction.fields = interaction.fields.fields;
+        try { modal.run(client, interaction, interaction.customId.split("-").slice(1).join("-")); } catch (e) {
+            client.emit("errorEmbed", interaction, e.stack);
         };
-    });
+    };
 };
