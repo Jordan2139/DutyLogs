f1f1fb7b3aa77809b976972a45b1e92ce1e2728a
Jordan.#2139 <jordansilva213@gmail.com>
2023-07-05 12:53:23 -0400

    create versioning

--- script/fxmanifest.lua
+++ script/fxmanifest.lua
@@ -1,13 +1,9 @@
 fx_version 'cerulean'
-games { 'gta5' }
+games {'gta5'}
 author 'Jordan.#2139'
+version '1.0.0'
 
-server_scripts {
-'server/*.js',
-'config.js',
-}
+server_scripts {'server/*.js', 'config.js'}
 
-client_scripts {
-'client/*.js'
-}
+client_scripts {'client/*.js'}
 
